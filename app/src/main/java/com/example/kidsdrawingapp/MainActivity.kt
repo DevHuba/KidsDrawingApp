066ffde5beda9c1bf705d71c53bf9231638e82c5
DevHuba <devhuba@gmail.com>
2022-04-07 11:11:59 +0300

    half of brush dialog logic

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -1,7 +1,10 @@
 package com.example.kidsdrawingapp
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.ImageButton
 import com.example.kidsdrawingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
@@ -13,8 +16,16 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
+        //Scale brush with default size of 20
         binding.drawingView.scaleBrush(20f)
 
+    }
+    
+    fun showBrushDialog () {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_brush_size)
+        brushDialog.setTitle("Brush size: ")
+        val smallBtn: ImageButton = brushDialog.findViewById(R.id.ibSmallBrush)
     }
 }
 
