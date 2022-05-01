015bf1ceab945205f2b6c8a072fcd15effbeded3
DevHuba <devhuba@gmail.com>
2022-05-01 19:19:24 +0300

    add multiple permissions option

--- app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
+++ app/src/main/java/com/example/kidsdrawingapp/MainActivity.kt
@@ -1,20 +1,40 @@
 package com.example.kidsdrawingapp
 
+import android.Manifest
+import android.app.AlertDialog
 import android.app.Dialog
-import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.ImageButton
 import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.core.view.get
 import com.example.kidsdrawingapp.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    //Multiple permission
+    private val storageResultLauncher: ActivityResultLauncher<Array<String>> =
+        registerForActivityResult(ActivityResultContracts.RequestMultiplePermissions()) { permissions ->
+            permissions.entries.forEach{
+                val permissionName = it.key
+                val isGranted = it.value
+                if (isGranted) {
+                    Toast.makeText(this, "Permission $permissionName Granted", Toast.LENGTH_SHORT).show()
+                } else {
+                    Toast.makeText(this, "Permission $permissionName Denied", Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
 
-    private lateinit var binding:ActivityMainBinding
+
+    //Variables
+    private lateinit var binding: ActivityMainBinding
     private var globalButtonCurrentPaint: ImageButton? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -27,7 +47,7 @@ class MainActivity : AppCompatActivity() {
         val llPaintColors = binding.llPaintColors
         globalButtonCurrentPaint = llPaintColors[1] as ImageButton
         globalButtonCurrentPaint!!.setImageDrawable(
-            ContextCompat.getDrawable(this,R.drawable.pallet_pressed)
+            ContextCompat.getDrawable(this, R.drawable.pallet_pressed)
         )
 
         binding.ibBrush.setOnClickListener {
@@ -35,56 +55,103 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.ibEraser.setOnClickListener {
-            showBrushDialog()
+            showEraserDialog()
             binding.drawingView.setColor("#ffffff")
         }
-
-
+        binding.ibImage.setOnClickListener {
+            requestStoragePermission()
+        }
     }
-    
-    private fun showBrushDialog () {
+
+    //Show rationale dialog for displaying why we need permission
+    private fun showRationaleDialog(title:String, message:String){
+        val builder: AlertDialog.Builder = AlertDialog.Builder(this)
+        builder.setTitle(title)
+            .setMessage(message)
+            .setPositiveButton("Cancel") { dialog, _ ->
+                dialog.dismiss()
+            }
+        builder.create().show()
+    }
+
+
+    //Show brush dialog to select brush size
+    private fun showBrushDialog() {
         val brushDialog = Dialog(this)
         brushDialog.setContentView(R.layout.dialog_brush_size)
         brushDialog.setTitle("Brush size: ")
         val smallBtn: ImageButton = brushDialog.findViewById(R.id.ibSmallBrush)
         val mediumBtn: ImageButton = brushDialog.findViewById(R.id.ibMediumBrush)
         val largeBtn: ImageButton = brushDialog.findViewById(R.id.ibLargeBrush)
-        smallBtn.setOnClickListener{
+        smallBtn.setOnClickListener {
             binding.drawingView.scaleBrush(10f)
             brushDialog.dismiss()
         }
-        mediumBtn.setOnClickListener{
+        mediumBtn.setOnClickListener {
             binding.drawingView.scaleBrush(20f)
             brushDialog.dismiss()
         }
-        largeBtn.setOnClickListener{
+        largeBtn.setOnClickListener {
             binding.drawingView.scaleBrush(30f)
             brushDialog.dismiss()
         }
         brushDialog.show()
     }
-    
-    fun paintClicked (view: View) {
+
+    //Show brush dialog to select brush size
+    private fun showEraserDialog() {
+        val brushDialog = Dialog(this)
+        brushDialog.setContentView(R.layout.dialog_eraser_size)
+        brushDialog.setTitle("Brush size: ")
+        val smallBtn: ImageButton = brushDialog.findViewById(R.id.ibSmallBrush)
+        val mediumBtn: ImageButton = brushDialog.findViewById(R.id.ibMediumBrush)
+        val largeBtn: ImageButton = brushDialog.findViewById(R.id.ibLargeBrush)
+        smallBtn.setOnClickListener {
+            binding.drawingView.scaleBrush(10f)
+            brushDialog.dismiss()
+        }
+        mediumBtn.setOnClickListener {
+            binding.drawingView.scaleBrush(20f)
+            brushDialog.dismiss()
+        }
+        largeBtn.setOnClickListener {
+            binding.drawingView.scaleBrush(100f)
+            brushDialog.dismiss()
+        }
+        brushDialog.show()
+    }
+
+    //Chose color from pallet
+    fun paintClicked(view: View) {
         if (view != globalButtonCurrentPaint) {
-            val imageButton  = view as ImageButton
+            val imageButton = view as ImageButton
             val colorTag = imageButton.tag.toString()
             binding.drawingView.setColor(colorTag)
 
-            imageButton.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.pallet_pressed))
+            imageButton.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.pallet_pressed))
 
-            globalButtonCurrentPaint?.setImageDrawable(ContextCompat.getDrawable(this,R.drawable.pallet_normal))
+            globalButtonCurrentPaint?.setImageDrawable(ContextCompat.getDrawable(this, R.drawable.pallet_normal))
 
             globalButtonCurrentPaint = view
 
-        } 
+        }
     }
     
+    private fun requestStoragePermission(){
+        if(ActivityCompat.shouldShowRequestPermissionRationale(
+                this,
+                Manifest.permission.READ_EXTERNAL_STORAGE)
+        ){
+            showRationaleDialog("Kids Drawing App","Kids Drawing App needs to Access Your External Storage")
+        }
+        else {
+            storageResultLauncher.launch(arrayOf(Manifest.permission.READ_EXTERNAL_STORAGE))
+        }
+    }
+
+
+
 }
-
-
-
-
-
 
 
 
